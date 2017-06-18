b99cdc85daf93ede044f6811978809a344365b2e
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-06-17 23:18:08 -0700

    m

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var port = process.env.PORT || 8000
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(port, function(){
+  console.log("listeing on" + port)
+});
